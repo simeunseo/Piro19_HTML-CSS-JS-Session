a6046e86ce446c8f081ecd35d49b0b5fd7697509
simeunseo <rkdcjfehowl@naver.com>
2023-07-04 18:58:13 +0900

    할일 추가 기능 스타일링

--- 실습원본/script.js
+++ 실습원본/script.js
@@ -1,36 +1,49 @@
 const listContainer = document.getElementById("list-container");
 
+// input에 입력한 할일을 추가하고 기존의 input창과 버튼은 삭제해주는 함수
 function addTask() {
+  // li 태그를 만든다.
   var li = document.createElement("li");
 
-  var input = document.getElementById("input-box");
-  var text = input.value;
+  // input 태그와 input에 입력된 값을 가져온다.
+  var input = document.getElementById("task-input");
+  var task = input.value;
 
+  // span 태그를 만들고 내용을 사용자가 입력한 task로 채운다.
   var span = document.createElement("span");
-  span.textContent = text;
+  span.innerText = task;
 
+  // li 태그의 자식으로 span 태그를 넣는다.
   li.appendChild(span);
 
+  // listContainer의 자식으로 li 태그를 넣는다.
   listContainer.appendChild(li);
 
-  // 기존의 <input> 태그와 <button> 태그를 제거합니다.
+  // 기존의 input 태그와 button 태그를 제거한다.
   listContainer.removeChild(input.parentNode);
 }
 
+// + 버튼을 클릭하면 입력창을 만들어주는 함수
 function createInput() {
+  // li 태그를 만든다.
   let li = document.createElement("li");
 
+  // input 태그를 만들고 type, id, placeholder를 지정해준다.
   let input = document.createElement("input");
   input.setAttribute("type", "text");
-  input.setAttribute("id", "input-box");
+  input.setAttribute("id", "task-input");
   input.setAttribute("placeholder", "할일을 입력해주세요");
 
+  // button 태그를 만들고 type, innerText, 이벤트리스너를 지정해준다.
   var button = document.createElement("button");
   button.setAttribute("type", "button");
-  button.textContent = "확인";
-  button.addEventListener("click", addTask);
+  button.innerText = "+";
+  button.addEventListener("click", addTask); // 버튼을 클릭하면 addTask함수가 실행된다.
 
+  // li 태그의 자식으로 input과 button을 차례로 넣는다.
   li.appendChild(input);
   li.appendChild(button);
+
+  // listContainer의 자식으로 li 태그를 넣는다.
   listContainer.appendChild(li);
 }
